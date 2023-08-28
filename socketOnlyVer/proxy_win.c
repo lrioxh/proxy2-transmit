034d14dcc1ce0b9ca5bfa9e26ac6c7b250e8fa7d
lrioxh <lrioxh@foxmail.com>
2023-08-28 10:51:26 +0800

    server & proxy for win(discard ver)

---
+++ socketOnlyVer/proxy_win.c
@@ -0,0 +1,164 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib,"ws2_32.lib")
+
+//const int BUFSZ = 100;
+#define BUFSZ 100
+#define BACKLOG 5	//最大监听数
+
+int main()
+{
+    WSADATA wsaData;
+    int wsaStarted = WSAStartup(MAKEWORD(2, 2), &wsaData); //目前建议使用最新2.2版本
+    SOCKET midSrvSocket = -1;
+    SOCKET midSrvConn = -1;
+    SOCKET midClntSocket = -1;
+    SOCKADDR_IN midSrvAddr = { 0 };
+    SOCKADDR_IN midClntAddr = { 0 };
+    SOCKADDR_IN clntAddr = { 0 };
+    int socklen = sizeof(SOCKADDR);
+    int recvLen = 0;
+    int iSend = 0;
+    //char toClntBuf[BUFSZ] = { 0 };
+    //char fromClntBuf[BUFSZ] = { 0 };
+    //char toSrvBuf[BUFSZ] = { 0 };
+    //char fromSrvBuf[BUFSZ] = { 0 };
+    char sendBuf[BUFSZ] = { 0 };
+    char recvBuf[BUFSZ] = { 0 };
+    char ipBuf[16] = { 0 };
+    UINT sPORT = 12322;
+    UINT cPORT = 12321;
+    char sIP[] = "127.0.0.3";
+
+    //as a client
+    inet_pton(AF_INET, sIP, &midClntAddr.sin_addr.S_un.S_addr);
+    //clientAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址
+    midClntAddr.sin_family = AF_INET;
+    midClntAddr.sin_port = htons(sPORT);
+
+    //as a server
+    midSrvAddr.sin_family = AF_INET;
+    midSrvAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址
+    midSrvAddr.sin_port = htons(cPORT);//绑定端口
+
+
+
+    //as client
+    if (wsaStarted == 0) {
+        midClntSocket = socket(AF_INET, SOCK_STREAM, 0);//协议族，Socket类型，创建了可识别套接字
+        //socke 返回类型SOCKETuint64 与printf接受int矛盾
+    }
+    if (midClntSocket == SOCKET_ERROR){
+        printf_s("mid: client socket error: %d\n", WSAGetLastError());
+        return -1;
+    }else {
+        printf_s("mid: client socket seccess: %d\n", midClntSocket);
+    }
+    //向服务器发出连接请求
+    if (
+        connect(midClntSocket, (SOCKADDR*)&midClntAddr, sizeof(midClntAddr))
+        == INVALID_SOCKET
+        ) {
+        //printf("%d", r);
+        printf("mid: Connect failed: %d", WSAGetLastError());
+        return -1;
+    }
+    recvLen = recv(midClntSocket, recvBuf, sizeof(recvBuf), 0);
+    if (recvLen > 0)
+        printf("mid recv: %s\n", recvBuf);
+    else if (recvLen == 0)
+        printf("mid: receive null");
+    else
+        printf("mid: receive failed");
+
+    //as server
+    if (wsaStarted == 0) {
+        midSrvSocket = socket(AF_INET, SOCK_STREAM, 0);//协议族，Socket类型，创建了可识别套接字
+        //socke 返回类型SOCKETuint64 与printf接受int矛盾
+    }
+    if (midSrvSocket == SOCKET_ERROR) {
+        printf_s("mid: server socket error: %d\n", WSAGetLastError());
+        return -1;
+    }else {
+        printf_s("mid: server socket seccess: %d\n", midSrvSocket);
+    }
+    //绑定
+    if (bind(midSrvSocket, (SOCKADDR*)&midSrvAddr, socklen) == SOCKET_ERROR) {
+        printf("mid: Failed bind:%d\n", WSAGetLastError());
+        return -1;
+    }
+    if (listen(midSrvSocket, BACKLOG) == SOCKET_ERROR) {
+        printf("mid: Listen failed:%d\n", WSAGetLastError());
+        return -1;
+    }//其中第二个参数代表能够接收的最多的连接数
+    printf_s("mid: waiting for client...\n");
+    //int len = sizeof(SOCKADDR);
+    ////第二次握手，通过accept来接受对方的套接字的信息
+    midSrvConn = accept(midSrvSocket, (SOCKADDR*)&clntAddr, &socklen);
+
+    if (midSrvConn == SOCKET_ERROR){
+        printf("mid: Accept failed:%d", WSAGetLastError());
+        return -1;
+    }else {
+        printf("mid: new client %d...\n", midSrvConn);
+    }
+    inet_ntop(AF_INET, &clntAddr.sin_addr.S_un.S_addr, ipBuf, sizeof(ipBuf));
+    sprintf_s(sendBuf, BUFSZ, "welcome %s to middle server", ipBuf);//找对对应的IP并且将这行字打印到那里
+    //发送信息
+    iSend = send(midSrvConn, sendBuf, strlen(sendBuf) + 1, 0);
+    if (iSend == SOCKET_ERROR) {
+        printf("mid: send failed");
+    }
+
+    while (1)
+    {
+
+        //from client
+        recvLen = recv(midSrvConn, recvBuf, sizeof(recvBuf), 0);
+        if (recvLen > 0) {
+            printf("mid from clent: (%d) %s\n", recvLen, recvBuf);
+            sprintf_s(sendBuf, BUFSZ, "(%s mid)", recvBuf);
+        }
+        else if (recvLen == 0) {
+            printf("mid receive null");
+            break;
+        }
+        else {
+            printf("mid receive failed");
+            break;
+        }
+        //to server
+        iSend = send(midClntSocket, sendBuf, strlen(sendBuf) + 1, 0);
+        if (iSend == SOCKET_ERROR) {
+            printf("mid: send failed");
+        }
+        //from server
+        recvLen = recv(midClntSocket, recvBuf, sizeof(recvBuf), 0);
+        if (recvLen > 0) {
+            printf("mid from server: (%d) %s\n", recvLen, recvBuf);
+            sprintf_s(sendBuf, BUFSZ, "(%s mid)", recvBuf);
+        }
+        else if (recvLen == 0) {
+            printf("mid receive null");
+            break;
+        }
+        else {
+            printf("mid receive failed");
+            break;
+        }
+        //to client
+        iSend = send(midSrvConn, sendBuf, strlen(sendBuf) + 1, 0);
+        if (iSend == SOCKET_ERROR) {
+            printf("mid: send failed");
+        }
+
+
+    }
+    closesocket(midSrvConn);//关闭
+    closesocket(midSrvSocket);
+    closesocket(midClntSocket);
+    WSACleanup();//释放资源的操作
+    return 0;
+}
---
+++ socketOnlyVer/server_win.c
@@ -0,0 +1,106 @@
+﻿
+#include <stdio.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib,"ws2_32.lib")
+
+
+#define PORT  4321		//端口号;
+#define BACKLOG 5	//最大监听数
+#define BUFSZ 4096
+
+int main()
+{
+
+    WSADATA wsaData;
+    int wsaStarted = WSAStartup(MAKEWORD(2, 2), &wsaData); //目前建议使用最新2.2版本
+    SOCKET servSocket = -1;
+    SOCKET servConn = -1;
+    SOCKADDR_IN servAddr = { 0 };
+    SOCKADDR_IN clientAddr = { 0 };
+    int socklen = sizeof(SOCKADDR);
+    int recvLen = 0;
+    int iSend = 0;
+    char sendBuf[BUFSZ]={0};
+    char recvBuf[BUFSZ]={0};
+    char ipBuf[16]={0};
+
+
+    if (wsaStarted == 0) {
+        servSocket = socket(AF_INET, SOCK_STREAM, 0);//协议族，Socket类型，创建了可识别套接字
+        //socke 返回类型SOCKETuint64 与printf接受int矛盾
+    }
+    //else {
+    //    int serSocket = -1;
+    //}
+    
+    if (servSocket == SOCKET_ERROR)
+    {
+        printf_s("创建socket失败！\n");
+        return 0;
+    }
+    else {
+        printf_s("成功创建套接字！%d\n", servSocket);
+    }
+
+    //需要绑定的参数，主要是本地的socket的一些信息。
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址
+    servAddr.sin_port = htons(PORT);//绑定端口
+    //addrServ.sin_addr.s_addr = inet_addr("127.0.0.1");    //将iP地址127.0.0.1也就是本机地址转换为十进制
+
+    //int retVal = bind(servSocket, (SOCKADDR*)&servAddr, sizeof(SOCKADDR));
+    if (bind(servSocket, (SOCKADDR*)&servAddr, socklen) == SOCKET_ERROR){
+        printf("Failed bind:%d\n", WSAGetLastError());
+        return;
+    }//绑定完成
+    if (listen(servSocket, BACKLOG) == SOCKET_ERROR) {
+        printf("Listen failed:%d", WSAGetLastError());
+        return;
+    }//其中第二个参数代表能够接收的最多的连接数
+    printf_s("等待客户端...\n");
+    //int len = sizeof(SOCKADDR);
+    ////第二次握手，通过accept来接受对方的套接字的信息
+    servConn = accept(servSocket, (SOCKADDR*)&clientAddr, &socklen);
+    ////如果这里不是accept而是conection的话。。就会不断的监听
+    if (servConn == SOCKET_ERROR)
+    {
+        printf("Accept failed:%d", WSAGetLastError());
+        return;
+    }
+    else {
+        printf("监听到新的客户端 %d...\n", servConn);
+    }
+    inet_ntop(AF_INET, &clientAddr.sin_addr.S_un.S_addr, ipBuf, sizeof(ipBuf));
+    sprintf_s(sendBuf, BUFSZ, "welcome %s to server", ipBuf);//找对对应的IP并且将这行字打印到那里
+    while (1)
+    {
+        //发送信息
+        iSend = send(servConn, sendBuf, strlen(sendBuf) + 1, 0);
+        if (iSend == SOCKET_ERROR) {
+            printf("send failed");
+            // break;
+        }
+        //char receiveBuf[100];//接收
+        //int RecvLen=0;
+        //memset(recvBuf, 0, sizeof(recvBuf));
+        recvLen = recv(servConn, recvBuf, sizeof(recvBuf), 0);
+
+        if (recvLen > 0) {
+            printf("%d %s\n", recvLen, recvBuf);
+            sprintf_s(sendBuf, BUFSZ, "(%s server)", recvBuf);
+        }
+        else if (recvLen == 0) {
+            printf("receive null");
+            break;
+        }
+        else {
+            printf("receive failed");
+            break;
+        }
+    }
+    closesocket(servConn);//关闭
+    closesocket(servSocket);
+    WSACleanup();//释放资源的操作
+    return 0;
+}
