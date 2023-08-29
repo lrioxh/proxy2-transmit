42aa296202c5c4459f1981e078b0dd33f7e58013
lrioxh <lrioxh@foxmail.com>
2023-08-29 17:24:40 +0800

    重构socket server & client for win

--- socketOnlyVer/client_win.c
+++ socketOnlyVer/client_win.c
@@ -5,94 +5,63 @@
 //#include <arpe/inet.h>
 #pragma comment(lib,"ws2_32.lib")
 
-#define BUFSZ                   (2048)
+#define BACKLOG                 (5)	//最大监听数
+#define BUFSZ                   (10240)
+#define PORT                    (4322)
 
 
-int main()
-{
+int main() {
 
     WSADATA wsaData;
-    int wsaStarted = WSAStartup(MAKEWORD(2, 2), &wsaData); //目前建议使用最新2.2版本
-    SOCKET clientSocket = -1;
-    //SOCKET servConn = -1;
-    //SOCKADDR_IN servAddr = { 0 };
-    SOCKADDR_IN clientAddr = { 0 };
-    //int socklen = sizeof(SOCKADDR);
-    int recvLen = 0;
-    char sendBuf[BUFSZ] = { 0 };
-    char recvBuf[BUFSZ] = { 0 };
-    char ipBuf[16] = { 0 };
-    char sIP[] = "192.168.232.128";
-    //socket编程中，它定义了一个结构体SOCKADDR_IN来存计算机的一些信息，像socket的系统，
-    //端口号，ip地址等信息，这里存储的是服务器端的计算机的信息
-    //SOCKADDR_IN clientAddr;
-    //clientAddr.sin_addr.S_un.S_addr = htonl("127.0.0.3");
-    inet_pton(AF_INET,"127.0.0.1", &clientAddr.sin_addr.S_un.S_addr);
-    //clientAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址
-    clientAddr.sin_family = AF_INET;
-    clientAddr.sin_port = htons(4322);
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+    char* buffer;
+    buffer = (char*)malloc(BUFSZ * sizeof(char));
 
-    if (wsaStarted == 0) {
-        clientSocket = socket(AF_INET, SOCK_STREAM, 0);//协议族，Socket类型，创建了可识别套接字
-        //socke 返回类型SOCKETuint64 与printf接受int矛盾
+    // 创建客户端socket
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
-    //else {
-    //    int serSocket = -1;
-    //}
 
-    if (clientSocket == SOCKET_ERROR)
-    {
-        printf("Socket() error:%d", WSAGetLastError());
-        return;
-    }
-    else {
-        printf_s("客户端套接字成功！%d\n", clientSocket);
+    // 设置服务器地址结构
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);  // 与服务器相同的端口
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);  // 服务器的IP地址
 
-        
-
-        //前期定义了套接字，定义了服务器端的计算机的一些信息存储在clientsock_in中，
-        //准备工作完成后，然后开始将这个套接字链接到远程的计算机
-        //也就是第一次握手
-        int r = connect(clientSocket, (SOCKADDR*)&clientAddr, sizeof(clientAddr));//开始连接
-            //向服务器发出连接请求
-        if (
-            //connect(clientSocket, (struct  sockaddr*)&clientAddr, sizeof(SOCKADDR))
-            r== INVALID_SOCKET
-            ) {
-            printf("%d", r);
-            printf("Connect failed:%d", WSAGetLastError());
-            return;
-        }
-        //else
-        //{
-        //    //接收数据
-        //    recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
-        //    printf("%s\n", recvBuf);
-        //}
-       // printf("%d\n",r);
-
-        while (1)
-        {
-            recvLen = recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
-            if (recvLen > 0)
-                printf("%s\n", recvBuf);
-            else if (recvLen == 0)
-                printf("receive null");
-            else
-                printf("receive failed");
-                //break;
-            //printf("%s\n", recvBuf);
-            gets(sendBuf);
-            if (strcmp(sendBuf, "quit") == 0)
-                break;
-            send(clientSocket, sendBuf, strlen(sendBuf) + 1, 0);
-        }
-
-        send(clientSocket, "", strlen(sendBuf) + 1, 0);
+    // 连接到服务器
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        perror("Connection failed");
         closesocket(clientSocket);
-        //关闭服务
-        WSACleanup();
-        return 0;
+        exit(EXIT_FAILURE);
     }
-}
 
+    printf("Connected to server\n");
+    printf("Type 'quit' to exit\n");
+
+    while (1) {
+        //char message[1024];            
+        memset(buffer, '\0', BUFSZ);
+        printf("Enter a message: ");
+        fgets(buffer, BUFSZ, stdin);
+
+        // 发送消息给服务器
+        send(clientSocket, buffer, strlen(buffer), 0);
+
+        // 退出条件
+        if (strncmp(buffer, "quit", 4) == 0) {
+            printf("Quitting...\n");
+            break;
+        }
+
+        // 接收服务器的响应
+        //char response[1024];
+        recv(clientSocket, buffer, BUFSZ, 0);
+        printf("Server response: %s\n", buffer);
+    }
+
+    closesocket(clientSocket);
+    return 0;
+}
--- socketOnlyVer/server_win.c
+++ socketOnlyVer/server_win.c
@@ -5,102 +5,84 @@
 #pragma comment(lib,"ws2_32.lib")
 
 
-#define PORT  4321		//端口号;
-#define BACKLOG 5	//最大监听数
-#define BUFSZ 4096
-
-int main()
-{
+#define BACKLOG                 (5)	//最大监听数
+#define BUFSZ                   (10240)
+#define PORT                    (4321)
+int main() {
 
     WSADATA wsaData;
-    int wsaStarted = WSAStartup(MAKEWORD(2, 2), &wsaData); //目前建议使用最新2.2版本
-    SOCKET servSocket = -1;
-    SOCKET servConn = -1;
-    SOCKADDR_IN servAddr = { 0 };
-    SOCKADDR_IN clientAddr = { 0 };
-    int socklen = sizeof(SOCKADDR);
-    int recvLen = 0;
-    int iSend = 0;
-    char sendBuf[BUFSZ]={0};
-    char recvBuf[BUFSZ]={0};
-    char ipBuf[16]={0};
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    int serverSocket, clientSocket;
+    int bytesRead=0;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
+    char* buffer;
+    buffer = (char*)malloc(BUFSZ * sizeof(char));
 
-
-    if (wsaStarted == 0) {
-        servSocket = socket(AF_INET, SOCK_STREAM, 0);//协议族，Socket类型，创建了可识别套接字
-        //socke 返回类型SOCKETuint64 与printf接受int矛盾
-    }
-    //else {
-    //    int serSocket = -1;
-    //}
-    
-    if (servSocket == SOCKET_ERROR)
-    {
-        printf_s("创建socket失败！\n");
-        return 0;
-    }
-    else {
-        printf_s("成功创建套接字！%d\n", servSocket);
+    // 创建服务器socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    //需要绑定的参数，主要是本地的socket的一些信息。
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址
-    servAddr.sin_port = htons(PORT);//绑定端口
-    //addrServ.sin_addr.s_addr = inet_addr("127.0.0.1");    //将iP地址127.0.0.1也就是本机地址转换为十进制
+    // 设置服务器地址结构
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);  // 选择一个合适的端口
 
-    //int retVal = bind(servSocket, (SOCKADDR*)&servAddr, sizeof(SOCKADDR));
-    if (bind(servSocket, (SOCKADDR*)&servAddr, socklen) == SOCKET_ERROR){
-        printf("Failed bind:%d\n", WSAGetLastError());
-        return;
-    }//绑定完成
-    if (listen(servSocket, BACKLOG) == SOCKET_ERROR) {
-        printf("Listen failed:%d", WSAGetLastError());
-        return;
-    }//其中第二个参数代表能够接收的最多的连接数
-    printf_s("等待客户端...\n");
-    //int len = sizeof(SOCKADDR);
-    ////第二次握手，通过accept来接受对方的套接字的信息
-    servConn = accept(servSocket, (SOCKADDR*)&clientAddr, &socklen);
-    ////如果这里不是accept而是conection的话。。就会不断的监听
-    if (servConn == SOCKET_ERROR)
-    {
-        printf("Accept failed:%d", WSAGetLastError());
-        return;
+    // 绑定socket到服务器地址
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        perror("Binding failed");
+        closesocket(serverSocket);
+        exit(EXIT_FAILURE);
     }
-    else {
-        printf("监听到新的客户端 %d...\n", servConn);
+
+    // 监听连接请求
+    if (listen(serverSocket, BACKLOG) == -1) {
+        perror("Listening failed");
+        closesocket(serverSocket);
+        exit(EXIT_FAILURE);
     }
-    inet_ntop(AF_INET, &clientAddr.sin_addr.S_un.S_addr, ipBuf, sizeof(ipBuf));
-    sprintf_s(sendBuf, BUFSZ, "welcome %s to server", ipBuf);//找对对应的IP并且将这行字打印到那里
-    while (1)
-    {
-        //发送信息
-        iSend = send(servConn, sendBuf, strlen(sendBuf) + 1, 0);
-        if (iSend == SOCKET_ERROR) {
-            printf("send failed");
-            // break;
+
+    printf("Server listening...\n");
+
+    while (1) {
+        // 接受客户端连接
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
+        if (clientSocket == -1) {
+            perror("Accepting connection failed");
+            continue;
         }
-        //char receiveBuf[100];//接收
-        //int RecvLen=0;
-        //memset(recvBuf, 0, sizeof(recvBuf));
-        recvLen = recv(servConn, recvBuf, sizeof(recvBuf), 0);
 
-        if (recvLen > 0) {
-            printf("%d %s\n", recvLen, recvBuf);
-            sprintf_s(sendBuf, BUFSZ, "(%s server)", recvBuf);
-        }
-        else if (recvLen == 0) {
-            printf("receive null");
-            break;
-        }
-        else {
-            printf("receive failed");
-            break;
+        printf("Client connected\n");
+
+        // 处理通信
+        while (1) {
+
+            memset(buffer, '\0', BUFSZ);
+            bytesRead = recv(clientSocket, buffer, BUFSZ, 0);
+            if (bytesRead <= 0) {
+                printf("Client disconnected\n");
+                closesocket(clientSocket);
+                break;
+            }
+            // 检查是否为关闭消息
+            if (strncmp(buffer, "quit", 4) == 0) {
+                printf("Client sent quit message\n");
+                // 这里可以进行一些清理工作，然后关闭连接
+                closesocket(clientSocket);
+                break;
+            }
+            // 在这里处理接收到的数据，然后构造要发送回客户端的响应
+            //sprintf(buffer, "%s-server", buffer);
+            sprintf(buffer + strlen(buffer)-1, "-server");
+            send(clientSocket, buffer, bytesRead+strlen("-server"), 0);
         }
     }
-    closesocket(servConn);//关闭
-    closesocket(servSocket);
+
+    closesocket(serverSocket);    
     WSACleanup();//释放资源的操作
+    free(buffer);
     return 0;
 }
