4e095f69815041467f064e0488c68adff899e3d9
lrioxh <lrioxh@foxmail.com>
2023-08-30 15:03:29 +0800

    优化close位置和send参数

--- socketOnlyVer/server_win.c
+++ socketOnlyVer/server_win.c
@@ -64,21 +64,22 @@ int main() {
             bytesRead = recv(clientSocket, buffer, BUFSZ, 0);
             if (bytesRead <= 0) {
                 printf("Client disconnected\n");
-                closesocket(clientSocket);
+                // closesocket(clientSocket);
                 break;
             }
             // 检查是否为关闭消息
             if (strncmp(buffer, "quit", 4) == 0) {
                 printf("Client sent quit message\n");
                 // 这里可以进行一些清理工作，然后关闭连接
-                closesocket(clientSocket);
+                // closesocket(clientSocket);
                 break;
             }
             // 在这里处理接收到的数据，然后构造要发送回客户端的响应
             //sprintf(buffer, "%s-server", buffer);
             sprintf(buffer + strlen(buffer)-1, "-server");
-            send(clientSocket, buffer, bytesRead+strlen("-server"), 0);
+            send(clientSocket, buffer, strlen(buffer), 0);
         }
+        closesocket(clientSocket);
     }
 
     closesocket(serverSocket);    
