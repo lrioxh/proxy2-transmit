810d245234044b987bf76ae767b702833ba99f74
lrioxh <lrioxh@foxmail.com>
2023-09-05 15:57:51 +0800

    尝试用非阻塞+事件监控代替阻塞如accpet，connect，recv

--- win/proxy2_transmit_pure.c
+++ win/proxy2_transmit_pure.c
@@ -5,7 +5,7 @@
 // 3.上传（c2s）循环阻塞在recv无法退出 链接设置非阻塞模式√超时break√
 // 4.openssl
 // 5.缓冲区队列?
-//
+// 6.非阻塞注册事件or阻塞-非阻收发-阻塞
 #include <stdio.h>
 #include <winsock2.h>
 #include <Ws2tcpip.h>
@@ -18,6 +18,38 @@
 #define PORT2SERV               (4321)
 #define PORT2CLNT               (4322)
 #define nonBlockMode            (1)
+
+int wsaTimeoutEvent(SOCKET proxySocket2Serv,DWORD time_out) {
+    WSAEVENT event = WSACreateEvent();
+    if (event == WSA_INVALID_EVENT) {
+        perror("WSACreateEvent failed");
+        return 1;
+    }
+
+    if (WSAEventSelect(proxySocket2Serv, event, FD_CONNECT) == SOCKET_ERROR) {
+        perror("WSAEventSelect failed");
+        WSACloseEvent(event);
+        return 1;
+    }
+    //DWORD time_out = 10000; // 设置连接超时为10秒
+    DWORD wait_result = WSAWaitForMultipleEvents(1, &event, FALSE, time_out, FALSE);
+    if (wait_result == WSA_WAIT_TIMEOUT) {
+        // 超时处理：连接未能在超时时间内完成
+        printf("Connection timed out\n");
+        return 1;
+    }
+    else if (wait_result == WSA_WAIT_EVENT_0) {
+        // 套接字可写，连接已建立
+        printf("Connected successfully\n");
+        return 0;
+    }
+    else {
+        perror("WSAWaitForMultipleEvents failed");
+        return 1;
+    }
+
+    WSACloseEvent(event);
+}
 
 int main()
 {
@@ -87,7 +119,11 @@ int main()
                 printf("New client %d...\n", proxyConn2Clnt);
             }
 
-            setsockopt(proxyConn2Clnt, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));//超时返回-1
+#if nonBlockMode
+            ioctlsocket(proxyConn2Clnt, FIONBIO, &nonBlockingMode);
+#endif
+            setsockopt(proxyConn2Clnt, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));//recv超时返回-1
+            setsockopt(proxyConn2Clnt, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));//send超时返回-1
 
             //as a client
             inet_pton(AF_INET, ipBuf, &proxyAddr2Serv.sin_addr.s_addr);
@@ -108,17 +144,52 @@ int main()
             else {
                 printf("client socket seccess: %d\n", proxySocket2Serv);
             }
-            setsockopt(proxySocket2Serv, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));//超时返回-1
+#if nonBlockMode
+            //ioctlsocket(proxySocket2Serv, FIONBIO, &nonBlockingMode);
+#endif
+            setsockopt(proxySocket2Serv, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+            setsockopt(proxySocket2Serv, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
             //向服务器发出连接请求
-            if (
-                connect(proxySocket2Serv, (struct sockaddr*)&proxyAddr2Serv, addrSize)
-                == INVALID_SOCKET
-                ) {
-                //printf("%d", r);
-                printf("Connect failed: %d\n", WSAGetLastError());
-                closesocket(proxySocket2Serv);//关闭
-                closesocket(proxyConn2Clnt);
-                continue;
+            int error_code = connect(proxySocket2Serv, (struct sockaddr*)&proxyAddr2Serv, addrSize);
+            //if (result == -1 && (errno == EINPROGRESS || errno == EWOULDBLOCK)) {
+            if (error_code !=0) {
+                error_code = WSAGetLastError();
+                if (error_code == WSAEWOULDBLOCK || error_code == WSAEINPROGRESS) {
+                    //WSAEVENT event = WSACreateEvent();
+                    //if (event == WSA_INVALID_EVENT) {
+                    //    perror("WSACreateEvent failed");
+                    //    continue;
+                    //}
+
+                    //if (WSAEventSelect(proxySocket2Serv, event, FD_CONNECT) == SOCKET_ERROR) {
+                    //    perror("WSAEventSelect failed");
+                    //    WSACloseEvent(event);
+                    //    continue;
+                    //}
+                    //DWORD time_out = 10000; // 设置连接超时为10秒
+                    //DWORD wait_result = WSAWaitForMultipleEvents(1, &event, FALSE, time_out, FALSE);
+                    //if (wait_result == WSA_WAIT_TIMEOUT) {
+                    //    // 超时处理：连接未能在超时时间内完成
+                    //    printf("Connection timed out\n");
+                    //    continue;
+                    //}
+                    //else if (wait_result == WSA_WAIT_EVENT_0) {
+                    //    // 套接字可写，连接已建立
+                    //    printf("Connected successfully\n");
+                    //}
+                    //else {
+                    //    perror("WSAWaitForMultipleEvents failed");
+                    //    continue;
+                    //}
+
+                    //WSACloseEvent(event);
+                }else {
+                    //printf("%d", r);
+                    printf("Connect failed: %d\n", WSAGetLastError());
+                    closesocket(proxySocket2Serv);//关闭
+                    closesocket(proxyConn2Clnt);
+                    continue;
+                }         
             }
 
             int recvBytes = 0;
@@ -129,63 +200,129 @@ int main()
                 memset(transBuf, 0, BUFSIZE);
 
                 //from client 阻塞
-                recvBytes = recv(proxyConn2Clnt, transBuf, BUFSIZE, 0);
-                if (recvBytes > 0) {
-                    printf("from clent: (%d)\n", recvBytes);
-                    //sprintf(sendBuf, BUFSIZE, "%s", recvBuf);
-                }
-                else {
-                    printf("connection closed\n");
-                    break;
-                }
+                // recvBytes = recv(proxyConn2Clnt, transBuf, BUFSIZE, 0);
+                // if (recvBytes > 0) {
+                //     printf("from clent: (%d)\n", recvBytes);
+                //     //sprintf(sendBuf, BUFSIZE, "%s", recvBuf);
+                // }
+                // else {
+                //     printf("connection closed\n");
+                //     break;
+                // }
 
 #if nonBlockMode
-                ioctlsocket(proxyConn2Clnt, FIONBIO, &nonBlockingMode);
+                // ioctlsocket(proxyConn2Clnt, FIONBIO, &nonBlockingMode);
 #endif
                 while (1) {
-                    //to server
-                    sendBytes = send(proxySocket2Serv, transBuf, recvBytes, 0);
-                    if (sendBytes == SOCKET_ERROR) {
-                        printf("send to server failed\n");
-                        break;
-                    }
-                    //Sleep(2);
-                    //from client
+                    // //to server
+                    // sendBytes = send(proxySocket2Serv, transBuf, recvBytes, 0);
+                    // if (sendBytes == SOCKET_ERROR) {
+                    //     printf("send to server failed\n");
+                    //     break;
+                    // }
+                    // //Sleep(2);
+                    // //from client
                     recvBytes = recv(proxyConn2Clnt, transBuf, BUFSIZE, 0);
-                    if (recvBytes > 0) {
-                        printf("from clent: (%d)\n", recvBytes);
-                        //sprintf(sendBuf, BUFSIZE, "%s", recvBuf);
-                    }
-                    else {
-                        printf("receive from clent finished\n");
+                    // if (recvBytes > 0) {
+                    //     printf("from clent: (%d)\n", recvBytes);
+                    //     //sprintf(sendBuf, BUFSIZE, "%s", recvBuf);
+                    // }
+                    // else {
+                    //     printf("receive from clent finished\n");
+                    //     break;
+                    // }
+
+                    if (recvBytes == -1) {
+                        //if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                        error_code = WSAGetLastError();
+                        if (error_code == WSAEWOULDBLOCK || error_code == WSAEINPROGRESS){
+                            // 套接字没有数据可读，继续循环
+                            continue;
+                        } else {
+                            // 处理接收错误
+                            printf("%s %d recv from clnt error=%d\n", __func__, __LINE__, errno);
+                            break;
+                        }
+                    } else if (recvBytes == 0) {
+                        // 对端关闭了连接
+                        printf("recv from clnt finished\n");
                         break;
+                    } else {
+                        // 处理接收到的数据
+                        printf("from clent: (%d)\n", recvBytes);
+                        // 发送数据
+                        sendBytes = send(proxySocket2Serv, transBuf, recvBytes, 0);
+                        if (sendBytes == -1) {
+                            //if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            error_code = WSAGetLastError();
+                            if (error_code == WSAEWOULDBLOCK || error_code == WSAEINPROGRESS) {
+                                // 套接字暂时无法发送数据，继续循环
+                                continue;
+                            } else {
+                                // 处理发送错误
+                                printf("%s %d send to serv error=%d\n", __func__, __LINE__, errno);
+                                break;
+                            }
+                        }
                     }
                 }
 #if nonBlockMode
-                ioctlsocket(proxyConn2Clnt, FIONBIO, &blockingMode);
+                // ioctlsocket(proxyConn2Clnt, FIONBIO, &blockingMode);
 #endif
                 //ioctlsocket(proxySocket2Serv, FIONBIO, &nonBlockingMode);
                 while (1) {
-                    //from server
+                    // //from server
                     recvBytes = recv(proxySocket2Serv, transBuf, BUFSIZE, 0);
-                    if (recvBytes > 0) {
-                        printf("from server: (%d)\n", recvBytes);
-                        //sprintf(sendBuf, BUFSIZE, "%s", recvBuf);
-                    }
-                    else {
-                        printf("receive from server finished\n");
+                    // if (recvBytes > 0) {
+                    //     printf("from server: (%d)\n", recvBytes);
+                    //     //sprintf(sendBuf, BUFSIZE, "%s", recvBuf);
+                    // }
+                    // else {
+                    //     printf("receive from server finished\n");
+                    //     break;
+                    // }
+                    // //to client
+                    // sendBytes = send(proxyConn2Clnt, transBuf, recvBytes, 0);
+                    // if (sendBytes == SOCKET_ERROR) {
+                    //     printf("send to client failed\n");
+                    //     break;
+                    // }
+                    if (recvBytes == -1) {
+                        //if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                        error_code = WSAGetLastError();
+                        if (error_code == WSAEWOULDBLOCK || error_code == WSAEINPROGRESS) {
+                            // 套接字没有数据可读，继续循环
+                            continue;
+                        } else {
+                            // 处理接收错误
+                            printf("%s %d recv from serv error=%d\n", __func__, __LINE__, errno);
+                            break;
+                        }
+                    } else if (recvBytes == 0) {
+                        // 对端关闭了连接
+                        printf("recv from serv finished\n");
                         break;
-                    }
-                    //to client
-                    sendBytes = send(proxyConn2Clnt, transBuf, recvBytes, 0);
-                    if (sendBytes == SOCKET_ERROR) {
-                        printf("send to client failed\n");
-                        break;
+                    } else {
+                        // 处理接收到的数据
+                        printf("from serv: (%d)\n", recvBytes);
+                        // 发送数据
+                        sendBytes = send(proxyConn2Clnt, transBuf, recvBytes, 0);
+                        if (sendBytes == -1) {
+                            //if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            error_code = WSAGetLastError();
+                            if (error_code == WSAEWOULDBLOCK || error_code == WSAEINPROGRESS) {
+                                // 套接字暂时无法发送数据，继续循环
+                                continue;
+                            } else {
+                                // 处理发送错误
+                                printf("%s %d send to clnt error=%d\n", __func__, __LINE__, errno);
+                                break;
+                            }
+                        }
                     }
                 }
 
                 //ioctlsocket(proxySocket2Serv, FIONBIO, &blockingMode);
-                //break;
             }
             free(transBuf);
             closesocket(proxyConn2Clnt);
